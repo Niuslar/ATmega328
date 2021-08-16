957aa24433272156aa75d7cf7c4e10fead9bbddf
Nico <nicolasuslar@gmail.com>
2021-08-16 14:29:40 +0100

    Add option to input multiple pins at once
    If the user wants to input multiple pins, they can use an or ( | ) operator.

--- Drivers/Src/gpio_driver.c
+++ Drivers/Src/gpio_driver.c
@@ -13,20 +13,20 @@ void gpio_init(pin_handle_t *pin_handle)
     //1. Select the mode (Input, Output)
     if(pin_handle->pin_config.pin_mode == OUTPUT)
     {
-        pin_handle->p_port_x->DDRx |= (1 << pin_handle->pin_config.pin_number);
+        pin_handle->p_port_x->DDRx |= pin_handle->pin_config.pin_number;
     }
     else if(pin_handle->pin_config.pin_mode == INPUT)
     {
-        pin_handle->p_port_x->DDRx &= ~(1 << pin_handle->pin_config.pin_number);
+        pin_handle->p_port_x->DDRx &= ~pin_handle->pin_config.pin_number;
 
         //2. Enable or Disable internal pull-up when the mode is INPUT
         if(pin_handle->pin_config.pin_pu == ENABLE)
         {
-            pin_handle->p_port_x->PORTx |= ( 1 << pin_handle->pin_config.pin_number);
+            pin_handle->p_port_x->PORTx |= pin_handle->pin_config.pin_number;
         }
         else
         {
-            pin_handle->p_port_x->PORTx &= ~( 1 << pin_handle->pin_config.pin_number);
+            pin_handle->p_port_x->PORTx &= ~pin_handle->pin_config.pin_number;
         }
     }
 
@@ -34,22 +34,36 @@ void gpio_init(pin_handle_t *pin_handle)
 
 void gpio_write_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number, uint8_t value)
 {
-    if(value == HIGH)
+    //Read register to get current status 
+    uint8_t temp_port = p_port_x->PORTx; 
+    
+    //Combine this with the new value 
+    if(value == LOW)
     {
-        p_port_x->PORTx |= (1 << pin_number);
+        temp_port &= ~(pin_number);
+        p_port_x->PORTx = temp_port;
     }
-    //NOTE: Numbers different from 1 will drive the pin LOW
+    //NOTE: Numbers different from 0 will drive the pin HIGH
     else
     {
-        p_port_x->PORTx &= ~(1 << pin_number);
+        temp_port |= pin_number;
+        p_port_x->PORTx = temp_port;
     }
 
 }
 
+//Note: This function can read multiple pins at once, but it will return only 0 (no pins HIGH) or 1 (at least one pin HIGH)
 uint8_t gpio_read_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number)
 {
     uint8_t value; 
-    value = (p_port_x->PINx >> pin_number) & 1;
-    return value;
+    value = p_port_x->PINx & pin_number;
+    if(value == LOW)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
