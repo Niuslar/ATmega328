0e6d4d04ce8338af36b468be4d705136d111bd70
Nico <nicolasuslar@gmail.com>
2021-08-16 19:17:41 +0100

    Now it is possible to use pin numbers directly or pin definitions
    to use change multiple pins at once, it is necessary to use pin definitions (PIN_NO_0) instead of the direct number.

--- Drivers/Src/gpio_driver.c
+++ Drivers/Src/gpio_driver.c
@@ -8,9 +8,14 @@
 
 #include "gpio_driver.h"
 
+static uint8_t pin_validation(uint16_t pin_int);
+
 void gpio_init(pin_handle_t *pin_handle)
 {
-    //1. Select the mode (Input, Output)
+    //Validate pin number 
+    pin_handle->pin_config.pin_number = pin_validation(pin_handle->pin_config.pin_number);
+    
+    //Select the mode (Input, Output)
     if(pin_handle->pin_config.pin_mode == OUTPUT)
     {
         pin_handle->p_port_x->DDRx |= pin_handle->pin_config.pin_number;
@@ -19,7 +24,7 @@ void gpio_init(pin_handle_t *pin_handle)
     {
         pin_handle->p_port_x->DDRx &= ~pin_handle->pin_config.pin_number;
 
-        //2. Enable or Disable internal pull-up when the mode is INPUT
+        //Enable or Disable internal pull-up when the mode is INPUT
         if(pin_handle->pin_config.pin_pu == ENABLE)
         {
             pin_handle->p_port_x->PORTx |= pin_handle->pin_config.pin_number;
@@ -32,8 +37,11 @@ void gpio_init(pin_handle_t *pin_handle)
 
 }
 
-void gpio_write_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number, uint8_t value)
+void gpio_write_digital_pin(gpio_reg_t *p_port_x, uint16_t pin_number, uint8_t value)
 {
+    //validate the pin number 
+    pin_number = pin_validation(pin_number);
+    
     //Read register to get current status 
     uint8_t temp_port = p_port_x->PORTx; 
     
@@ -53,8 +61,11 @@ void gpio_write_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number, uint8_t va
 }
 
 //Note: This function can read multiple pins at once, but it will return only 0 (no pins HIGH) or 1 (at least one pin HIGH)
-uint8_t gpio_read_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number)
+uint8_t gpio_read_digital_pin(gpio_reg_t *p_port_x, uint16_t pin_number)
 {
+    //validate the pin number 
+    pin_number = pin_validation(pin_number);
+    
     uint8_t value; 
     value = p_port_x->PINx & pin_number;
     if(value == LOW)
@@ -65,5 +76,40 @@ uint8_t gpio_read_digital_pin(gpio_reg_t *p_port_x, uint8_t pin_number)
     {
         return 1;
     }
+}
+
+//If the user enters a pin number without using the definitions, this function will return the corresponding def
+static uint8_t pin_validation(uint16_t pin_int)
+{
+    if(pin_int < 7)
+    {
+        switch(pin_int)
+        {
+            case 0:
+                return (PIN_NO_0 >> 3);
+            case 1:
+                return (PIN_NO_1 >> 3);
+            case 2:
+                return (PIN_NO_2 >> 3);
+            case 3:
+                return (PIN_NO_3 >> 3);
+            case 4: 
+                return (PIN_NO_4 >> 3);
+            case 5: 
+                return (PIN_NO_5 >> 3); 
+            case 6:
+                return (PIN_NO_6 >> 3);
+            case 7:
+                return (PIN_NO_7 >> 3);
+            default: 
+                return 99;
+        }
+    }
+    
+    else
+    {
+        return pin_int >> 3;
+    }
+    
 }
 
