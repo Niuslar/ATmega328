f5131adeb64575bafa64f28bae3153cddc4592b3
Nico <nicolasuslar@gmail.com>
2021-08-25 13:26:46 +0100

    Add 16-bit timer bits definitions

--- Drivers/Inc/interrupts.h
+++ Drivers/Inc/interrupts.h
@@ -40,17 +40,16 @@
 
 #define _VECTOR(N) __vector_ ## N
 
-#  define ISR(vector, ...)            \
+#define ISR(vector, ...)            \
 void vector (void) __attribute__ ((signal,used, externally_visible)) __VA_ARGS__; \
 void vector (void)
 
 //Registers bit definitions
-#define TIMSK0  *((volatile uint8_t *)0x6E)
 
 #define SREG    *((volatile uint8_t *)0x5F)
 
 //Bits in TCCR0A
-#define WGM01  0
+#define WGM00  0
 #define WGM01  1
 #define COM0B0 4
 #define COM0B1 5
@@ -69,6 +68,21 @@ void vector (void)
 #define TOIE0   0
 #define OCIE0A  1
 #define OCIE0B  2
+
+//Bits in TCCR1B 
+#define CS10  0
+#define CS11  1
+#define CS12  2
+#define WGM12 3
+#define WGM13 4
+#define ICES1 6
+#define ICNC1 7
+
+//Bits in TIMSK1 
+#define TOIE1  0 
+#define OCIE1A 1
+#define OCIE1B 2
+#define ICIE1  5
 
 //I is the bit that enables interrupts in the SREG (Status Register) 
 #define I    7
