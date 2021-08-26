4ba8fb27fb047d4794517744ba8703e686c1b174
Nico <nicolasuslar@gmail.com>
2021-08-26 20:50:20 +0100

    Update timers.c

--- Drivers/Src/timers.c
+++ Drivers/Src/timers.c
@@ -14,9 +14,7 @@ static uint32_t timer0_fract = 0;
 #define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
 #define clockCyclesToMicroseconds(a) ( ((a) * 1000L) / (F_CPU / 1000L) )
 #define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
-
 #define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-
 #define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
 #define FRACT_MAX (1000 >> 3)
 
